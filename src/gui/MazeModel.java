d45b99508803427f3fc8b11bba2325371a1c84c7
Vincent Josse <vincent.josse@telecom-paris.fr>
2019-10-26 20:14:29 +0200

    Added mazeModel

---
+++ src/gui/MazeModel.java
@@ -0,0 +1,15 @@
+package gui;
+
+import java.awt.*;
+import javax.swing.*;
+import maze.*;
+
+public final class MazeModel {
+	
+	private Maze maze;
+	
+	public MazeModel(Maze maze) {
+		this.maze = maze;
+	}
+
+}
