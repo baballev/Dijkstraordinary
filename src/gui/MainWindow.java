0c1ad1d56ef9eb0234d2c3126b0b52647ba49ecb
Vincent Josse <vincent.josse@telecom-paris.fr>
2019-10-26 19:37:54 +0200

    The window is now ready to be programmed

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 public class MainWindow extends JFrame {
 
 	private final MenuBar menuBar;
+	private final WindowPanel windowPanel;
 	
 	public MainWindow() {
 		super("Labyrinthe");
@@ -12,10 +13,13 @@ public class MainWindow extends JFrame {
 		menuBar = new MenuBar(this);
 		setJMenuBar(menuBar);
 		
+		windowPanel = new WindowPanel(this);
+		this.setContentPane(windowPanel);
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		pack();
-		setVisible(true);
+		this.pack();
+		this.setVisible(true);
 		
 	}
 }
--- src/gui/WindowPanel.java
+++ src/gui/WindowPanel.java
@@ -1,5 +1,16 @@
 package gui;
 
-public class WindowPanel {
+import javax.swing.*;
+import java.awt.*;
 
+public final class WindowPanel extends JPanel{
+	private final MazePanel mazePanel;
+	
+	public WindowPanel(MainWindow mainWindow) {
+		super();
+		
+		this.setLayout(new BorderLayout());
+		
+		this.add(mazePanel = new MazePanel(mainWindow), BorderLayout.CENTER);
+	}
 }
