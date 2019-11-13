2329666888ccb3f6406408ce98a1cd9b0c3ae991
Vincent Josse <vincent.josse@telecom-paris.fr>
2019-11-13 21:49:44 +0100

    Working maze editor!

--- src/gui/SaveMenuItem.java
+++ src/gui/SaveMenuItem.java
@@ -1,10 +1,11 @@
 package gui;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public final class SaveMenuItem extends JMenuItem {
+public final class SaveMenuItem extends JMenuItem implements ActionListener {
 
 	private final MainWindow mainWindow;
 	
@@ -12,6 +13,15 @@ public final class SaveMenuItem extends JMenuItem {
 		super("Enregistrer...");
 		this.mainWindow = mainWindow;
 		this.setAccelerator(KeyStroke.getKeyStroke('S', ActionEvent.CTRL_MASK));
+		this.addActionListener(this);
 	}
+	
+	public final void actionPerformed(ActionEvent e) {
+		JFileChooser saveChooser = new JFileChooser("data/");
+		saveChooser.setDialogTitle("Sélectionner le fichier du labyrinthe");
+		if(saveChooser.showSaveDialog(null) == JFileChooser.APPROVE_OPTION) {
+			this.mainWindow.getMazeModel().getMaze().saveFromTextFile(saveChooser.getSelectedFile().getAbsolutePath());
+		}
+		}
 	
 }
